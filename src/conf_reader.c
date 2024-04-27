4324d42613bb9c0a1df60ce0c1cfdca0555c264f
Adam Leadbetter <adamml@users.noreply.github.com>
2024-04-27 22:53:30 +0100

    Removing warnings

--- src/conf_reader.c
+++ src/conf_reader.c
@@ -12,21 +12,21 @@ void conf_reader(struct conf *c, char* f){
                 if(strncmp(line, "site_id = ", 10) == 0){
                     subString = strtok(line, "\"");
                     subString = strtok(NULL, "\"");
-                    if(subString){strncpy(c->AUTARCO_SITE_ID, subString, 64);}
+                    if(subString){strncpy(c->AUTARCO_SITE_ID, subString, 63);}
                 } else if (strncmp(line, "uname = ", 8) == 0)
                 {
                     subString = strtok(line, "\"");
                     subString = strtok(NULL, "\"");
-                    if(subString){strncpy(c->AUTARCO_UNAME, subString, 256);}
+                    if(subString){strncpy(c->AUTARCO_UNAME, subString, 255);}
                 } else if (strncmp(line, "pwd = ", 6) == 0){
                     subString = strtok(line, "\"");
                     subString = strtok(NULL, "\"");
-                    if(subString){strncpy(c->AUTARCO_PWD, subString, 256);}
+                    if(subString){strncpy(c->AUTARCO_PWD, subString, 255);}
                 } else if (strncmp(line, "outdir = ", 9) == 0)
                 {
                     subString = strtok(line, "\"");
                     subString = strtok(NULL, "\"");
-                    if(subString){strncpy(c->AUTARCO_OUT_DIR, subString, 512);}
+                    if(subString){strncpy(c->AUTARCO_OUT_DIR, subString, 511);}
                 }
             }
             if(strncmp(line, "[autarco]", 9) == 0){
